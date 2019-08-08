98f434c31886cf5b9c250ea5f9e1e67051d98842
qsh <510756382@qq.com>
2019-08-08 17:59:35 +0800

    add user service to configService

--- springboot-crud-demo/src/main/java/com/qsh/springbootcruddemo/SpringbootCrudDemoApplication.java
+++ springboot-crud-demo/src/main/java/com/qsh/springbootcruddemo/SpringbootCrudDemoApplication.java
@@ -2,13 +2,14 @@ package com.qsh.springbootcruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableAsync
+@EnableEurekaClient
 public class SpringbootCrudDemoApplication {
 
     public static void main(String[] args) {
